9d76eb6cf22841193e551cc32a17695b759cf3d9
Hui-Zhi <zhaohuizhi@gmail.com>
2021-01-11 16:45:22 +0800

    add export to html

--- utils/export.py
+++ utils/export.py
@@ -1,5 +1,10 @@
 from rich.console import Console
 from rich.table import Column, Table
+import pandas as pd
+from jinja2 import Environment, ChoiceLoader, FileSystemLoader
+from IPython.display import HTML
+from pandas.io.formats.style import Styler
+
 
 def rpms_export_to_terminal(df):
     console = Console()
@@ -22,16 +27,74 @@ def rpms_export_to_terminal(df):
     
     console.print(table)
 
+def set_rpm_format_for_html(column):
+    if "Not supported" in column:
+        return "background-color: red"
+    elif "Supported by both SUSE and the vendor" in column:
+        return "background-color: blue"
+    elif "Supported by SUSE" in column:
+        return "background-color: green"
+    
+    return "background-color: white"
+
+def get_html_render_styles():
+    styles = [
+        dict(selector='table', props=[('border', '1px solid green'), 
+                                      ('border-collapse', 'collapse')]),
+        dict(selector='th', props=[('font-size', '12pt'), 
+                                   ('font-family', 'arial'), 
+                                   ('background-color', 'green'),
+                                   ('border', '1px solid green')]),
+        dict(selector='td', props=[('font-size', '10pt'), 
+                                   ('font-family', 'arial'), 
+                                   ('border', '1px solid green')])
+    ]
+    
+    return styles
+
 def rpms_export_to_html(df, file):
-    df.to_html(file, index=False)
+    df["Driver Support Status"] = df["Driver Support Status"].str.replace('\n', '</br>')
+    df["Driver Support Status"] = df["Driver Support Status"].str.replace('\t', '&nbsp&nbsp&nbsp&nbsp')
+    s = df.style.applymap(set_rpm_format_for_html, subset = pd.IndexSlice[:, ['Driver Support Status']]).hide_index()
+
+    styles = get_html_render_styles()
+
+    s = s.set_table_styles(styles)
+
+    with open(file, 'w') as f:
+        f.write(s.render())
 
 def rpms_export_to_pdf(df, file):
     df.to_pdf(file, index=False)
 
 def rpms_export_to_excel(df, file):
-    df.to_excel(file, index=False)
+    writer = pd.ExcelWriter(file, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Solid Driver Checks')
+    workbook = writer.book
 
-def get_support_flag_with_color(support_flag):
+    # workbook = load_workbook(file)
+    worksheet = writer.sheets['Solid Driver Checks']
+    red_format = workbook.add_format({'bg_color':'red'})
+    green_format = workbook.add_format({'bg_color':'green'})
+    blue_format = workbook.add_format({'bg_color':'blue'})
+
+    area = 'E2:E' + str(len(df.index) + 1)
+    worksheet.conditional_format(area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'Not supported',
+                                        'format': red_format})
+    worksheet.conditional_format(area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'Supported by both SUSE and the vendor',
+                                        'format': blue_format})
+    worksheet.conditional_format(area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'Supported by SUSE',
+                                        'format': green_format})
+
+    writer.save()
+
+def get_support_flag_with_color_for_rich(support_flag):
     if support_flag == 'yes':
         return '[green]Supported by SUSE[/green]'
     elif support_flag == 'external':
@@ -39,13 +102,13 @@ def get_support_flag_with_color(support_flag):
     else:
         return '[red]N/A[/red]'
 
-def get_running_with_color(running):
+def get_running_with_color_for_rich(running):
     if running == "True":
         return '[green]True[/green]'
     else:
         return '[gray]False[/gray]'
 
-def get_rpm_information_with_color(rpm_info):
+def get_rpm_information_with_color_for_rich(rpm_info):
     if 'is not owned by any package' in rpm_info:
         return '[red]' + rpm_info + '[/red]'
     else:
@@ -61,19 +124,91 @@ def os_export_to_terminal(df):
     table.add_column("RPM Information")
 
     for _, row in df.iterrows():
-        support_flag = get_support_flag_with_color(row['Support Flag'])
-        running = get_running_with_color(row['Running'])
-        rpm_info = get_rpm_information_with_color(row['RPM Information'])
+        support_flag = get_support_flag_with_color_for_rich(row['Support Flag'])
+        running = get_running_with_color_for_rich(row['Running'])
+        rpm_info = get_rpm_information_with_color_for_rich(row['RPM Information'])
 
         table.add_row(row['Name'], row['Path'], support_flag, running, rpm_info)
     
     console.print(table)
 
+def set_drivers_support_flag_format_for_html(col):
+    if col == 'yes':
+        return "background-color: green"
+    elif col == 'external':
+        return "background-color: blue"
+    elif col == 'N/A':
+        return "background-color: red"
+    
+    return ""
+
+def set_drivers_running_format_for_html(col):
+    if col == 'True':
+        return "background-color: green"
+    
+    return "background-color: gray"
+
+def set_drivers_rpm_info_format_for_html(col):
+    if 'is not owned by any package' in col:
+        return "background-color: red"
+    
+    return ""
+
 def os_export_to_html(df, file):
-    df.to_html(file, index=False)
+    s = df.style.applymap(set_drivers_support_flag_format_for_html, subset = pd.IndexSlice[:, ['Support Flag']])
+    s = s.applymap(set_drivers_running_format_for_html, subset = pd.IndexSlice[:, ['Running']])
+    s = s.applymap(set_drivers_rpm_info_format_for_html, subset = pd.IndexSlice[:, ['RPM Information']]).hide_index()
+
+    styles = get_html_render_styles()
+    
+    s = s.set_table_styles(styles)
+
+    with open(file, 'w') as f:
+        f.write(s.render())
 
 def os_export_to_pdf(df, file):
     df.to_pdf(file, index=False)
 
 def os_export_to_excel(df, file):
-    df.to_excel(file, index=False)
+    writer = pd.ExcelWriter(file, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Solid Driver Checks')
+    workbook = writer.book
+
+    worksheet = writer.sheets['Solid Driver Checks']
+    red_format = workbook.add_format({'bg_color':'red'})
+    green_format = workbook.add_format({'bg_color':'green'})
+    blue_format = workbook.add_format({'bg_color':'blue'})
+    gray_format = workbook.add_format({'bg_color':'gray'})
+
+    records = str(len(df.index) + 1)
+    support_flag_area = 'C2:C' + records
+    running_area = 'D2:D' + records
+    rpm_info_area = 'E2:E' + records
+    worksheet.conditional_format(support_flag_area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'N/A',
+                                        'format': red_format})
+    worksheet.conditional_format(support_flag_area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'external',
+                                        'format': blue_format})
+    worksheet.conditional_format(support_flag_area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'yes',
+                                        'format': green_format})
+
+    worksheet.conditional_format(running_area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'True',
+                                        'format': green_format})
+    worksheet.conditional_format(running_area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'False',
+                                        'format': gray_format})
+    
+    worksheet.conditional_format(rpm_info_area, {'type': 'text',
+                                        'criteria': 'containing',
+                                        'value': 'is not owned by any package',
+                                        'format': red_format})
+
+    writer.save()
