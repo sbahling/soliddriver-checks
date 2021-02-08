0394c4b2ef54530b0926cabb5454cdfdeb4a5072
Scott Bahling <sbahling@suse.com>
2021-02-08 18:59:20 +0100

    PEP-8: Fix formatting and code style to PEP-8 standards
    * Remove extra spaces
    * Wrap strings with '
    * 2 lines between module level function and class definitions
    * Avoid * with imports
    * Wrap some long lines
    * Ensure proper indentation of wrapped lines

--- utils/input_data.py
+++ utils/input_data.py
@@ -1,20 +1,24 @@
 import os
 
+
 def get_drivers_from_dir(path):
     exit(0)
+
 
 def get_rpms_from_dir(path):
     rpms = []
     for root, _, files in os.walk(path):
         for rpm in files:
-            if rpm.endswith(".rpm"):
+            if rpm.endswith('.rpm'):
                 rpmpath = os.path.join(root, rpm)
                 rpms.append(rpmpath)
-    
+
     return rpms
+
 
 def get_drivers_from_os():
     exit(0)
+
 
 def get_drivers_from_remote(ip, port, username, password):
     exit(0)
