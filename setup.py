319b7a338565c3540cb4b97087766add0ddce4fb
Hui-Zhi <zhaohuizhi@gmail.com>
2021-04-01 18:17:58 +0800

    add version to option

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__VERSION__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+
 setup(
     name="soliddriver-checks",
-    version="0.0.18",
+    version=get_version("src/soliddriver_checks/version.py"),
     author="Hui-Zhi Zhao",
     author_email="hui.zhi.zhao@suse.com",
     description=("Check RPM(s) and Drivers information"),
@@ -33,6 +51,10 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         'click',
+        'paramiko',
+        'pdfkit',
+        'dominate',
+        'rich'
     ],
     entry_points={
         'console_scripts': [
