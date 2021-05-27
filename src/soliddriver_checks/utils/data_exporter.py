567e8df8f39f2c53e478928ee31131e1e6039d2d
Hui-Zhi <zhaohuizhi@gmail.com>
2021-05-27 15:49:32 +0800

    only highlight the failed driver supported flag

--- src/soliddriver_checks/utils/data_exporter.py
+++ src/soliddriver_checks/utils/data_exporter.py
@@ -1,25 +1,30 @@
 import pdfkit
 import pandas as pd
 import os
-from dominate.tags import html, body, h1, div
+import dominate
+from dominate.tags import html, body, h1, div, tr, td, table
 from dominate.util import raw
-from openpyxl.styles import PatternFill, Font
+from openpyxl.styles import PatternFill, Font, Alignment
+from openpyxl.styles.borders import Border, Side
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl import load_workbook
 from openpyxl.formatting.rule import Rule
 import json
+import ast
+import string
+from openpyxl import Workbook
 import time
 # import importlib_resources
-import pkgutil
+# import pkgutil
 
 
 class FormatConfig:
     def __init__(self):
-        # config = importlib_resources.files('soliddriver_checks') / "config"
-        # conf_buffer = (config / "soliddriver-checks.conf").read_text()
-        conf_buffer = pkgutil.get_data("soliddriver_checks", "config/soliddriver-checks.conf")
+        pkg_path = os.path.dirname(__file__)
+        cfg_path = f"{pkg_path}/../config/soliddriver-checks.conf"
 
-        self._formatting = json.loads(conf_buffer)
+        with open(cfg_path, 'r') as fp:
+            self._formatting = json.load(fp)
 
     def load_body_format(self):
         return self._formatting["body"]
@@ -74,13 +79,13 @@ class RPMsExporter:
         formatting = self._formatting.load_support_flag_format()
         bgcolor_missing = formatting['Missing']['background-color']
         bgcolor_yes = formatting['yes']['background-color']
-        bgcolor_external = formatting['external']['background-color']
+        # bgcolor_external = formatting['external']['background-color']
         if ': no' in value or ': Missing' in value:
             return 'background-color: ' + bgcolor_missing
         elif ': yes' in value:
             return 'background-color: ' + bgcolor_yes
-        elif ': external' in value:
-            return 'background-color: ' + bgcolor_external
+        # elif ': external' in value:
+        #     return 'background-color: ' + bgcolor_external
         else:
             return 'background-color: white'
 
@@ -93,47 +98,195 @@ class RPMsExporter:
         return 'background-color: %s' % bgcolor
 
 
+    def _highlight_supported_error(self, row):
+        return ['background-color: #CD5C5C; color: white' if row[1] != 'external' else '' for r in row]
+
+    def _fmt_supported_to_html(self, val):
+        if val == '':
+            return ''
+        else:
+            val = ast.literal_eval(val)
+            df = pd.DataFrame.from_dict(val, orient='index')
+            s = df.style.apply(self._highlight_supported_error, axis=1).\
+                hide_index()
+            return s.render(head=[])
+
+    def _fmt_driver_symbol_result(self, val):
+        unfound = ""
+        for i in range(len(val['unfound'])):
+            if i % 5 == 0:
+                unfound += "\n"
+            unfound += val['unfound'][i] + ", "
+
+        mismatch = ""
+        for i in range(len(val['checksum-mismatch'])):
+            if i % 5 == 0:
+                mismatch += "\n"
+            mismatch += val['checksum-mismatch'][i] + ", "
+
+        return "Can't find symbols in RPM: \n" + unfound + "\n\n" + "symbols check sum doesn't match in RPM: \n" + mismatch
+
+    def _fmt_symbol_check_to_html(self, val):
+        if val == '':
+            return '{}'
+
+        val = ast.literal_eval(val)
+        for key in val:
+            val[key]['symbol check'] = self._fmt_driver_symbol_result(val[key]['symbol check'])
+            val[key]['symbol check'] = val[key]['symbol check'].replace('\n', '</br>')
+
+        df = pd.DataFrame.from_dict(val, orient='index')
+        return df.style.hide_index().\
+                  set_table_styles([dict(selector='table', props=[('border','1px solid red')]),
+                                    dict(selector='th', props=[('border','1px solid red')]),
+                                    dict(selector='td', props=[('border','1px solid red'),
+                                                               ('background-color','#CD5C5C'),
+                                                               ('color','white')])]).render(head=[])
+
+    def _error_in_supported(self, val):
+        if val == '':
+            return False
+        else:
+            val = ast.literal_eval(val)
+            for key in val:
+                if val[key]['assessment'] == 'Missing' or val[key]['assessment'] == 'yes':
+                    return True
+
+        return False
+
     def to_html(self, rpm_table, file):
         tableFormatter = HTMLTableFormatting()
         styles = tableFormatter.get_style()
 
-        rpm_table['Driver Flag: supported'] = rpm_table['Driver Flag: supported'].str.replace('\n', '</br>')
-        rpm_table['Symbols Check'] = rpm_table['Symbols Check'].str.replace('\n', '</br>')
+        rpm_table['Driver Flag: supported'] = rpm_table['Driver Flag: supported'].astype(str)
+        rpm_table['Symbols Check'] = rpm_table['Symbols Check'].astype(str)
+        report = dominate.document(title='checking result from soliddriver-checks')
 
-        s = rpm_table.style.\
-            applymap(self._supported_render,
-                     subset=pd.IndexSlice[:, ['Driver Flag: supported']],
-                    ).hide_index().\
-            applymap(self._symbol_render,
-                     subset=pd.IndexSlice[:, ['Symbols Check']],
-                    ).hide_index().\
-            set_table_styles(styles)
+        table_style = f"border:1px solid green;font-size:12px;font-family:sans-serif"
+        row_style = f"{table_style}"
+        row_warning_style = f"{table_style};border-color: red"
+        cell_style = row_style
+        cell_warning_style= f"{table_style};border-color: red"
+        cell_warn_red_style = f"border:1px solid red;font-size:12px;font-family:sans-serif"
+        header_style = f"{table_style}"
+        with report:
+            with table(style=table_style):
+                with tr():
+                    cols = rpm_table.columns
+                    for col in cols:
+                        td(col, style=header_style)
+
+                for i, row in rpm_table.iterrows():
+                    supported = row['Driver Flag: supported']
+                    symbol_check = row['Symbols Check']
+                    curr_row_style = row_style
+                    if self._error_in_supported(supported) or symbol_check != '':
+                        curr_row_style = row_warning_style
+                    with tr(style=curr_row_style):
+                        for c in cols:
+                            val = row[c]
+                            if c == 'Driver Flag: supported':
+                                if self._error_in_supported(val):
+                                    td(raw(self._fmt_supported_to_html(val)), style=cell_warning_style)
+                                else:
+                                    td(raw(self._fmt_supported_to_html(val)), style=cell_style)
+                            elif c == 'Symbols Check':
+                                if val != '{}':
+                                    td(raw(self._fmt_symbol_check_to_html(val)), style=cell_warn_red_style)
+                                else:
+                                    td("", style=cell_style)
+                            else:
+                                td(val, style=cell_style)
 
         with open(file, 'w') as f:
-            f.write(s.render())
+            f.write(report.render())
+
+        return
 
     def to_json(self, rpm_table, file):
         rpm_table.to_json(file, orient='records')
 
     def to_excel(self, rpm_table, file):
-        writer = pd.ExcelWriter(file, engine='openpyxl')
         if os.path.exists(file):
-            writer = pd.ExcelWriter(file, engine='openpyxl', mode='a')
+            os.remove(file)
 
-        sheet_name = 'Solid Driver Checks'
-        rpm_table.to_excel(writer,
-                           index=False,
-                           sheet_name=sheet_name)
-        writer.save()
-        writer.close()
+        rpm_table['Driver Flag: supported'] = rpm_table['Driver Flag: supported'].astype(str)
+        rpm_table['Symbols Check'] = rpm_table['Symbols Check'].astype(str)
 
-        workbook = load_workbook(filename=file)
-        worksheet = workbook[sheet_name]
+        cols = rpm_table.columns
+        xlsx_cols = list(string.ascii_lowercase[0:len(cols)])
+
+        wb = Workbook()
+        ws = wb.active
+        ws.title = 'solid driver checks'
+
+        general_border = Border(left=Side(border_style="thin",
+                                          color='00339966'),
+                                right=Side(border_style="thin",
+                                          color='00339966'),
+                                top=Side(border_style="thin",
+                                         color='00339966'),
+                                bottom=Side(border_style="thin",
+                                            color='00339966'))
+        general_align = Alignment(horizontal='left',vertical='top',wrap_text=True)
+
+        for i in range(len(xlsx_cols)):
+            cell_no = xlsx_cols[i] + '1'
+            ws[cell_no] = cols[i]
+            ws[cell_no].border = general_border
+            ws[cell_no].alignment = general_align
+
+        curr_row_no = 2
+        supported_col_no = 0
+
+        for i in range(len(cols)):
+            if cols[i] == "Driver Flag: supported":
+                supported_col_no = i
+                break
+
+        for i, row in rpm_table.iterrows():
+            for col_idx in range(len(cols)):
+                cell_no = xlsx_cols[col_idx] + str(curr_row_no)
+                val = row[cols[col_idx]]
+                if cols[col_idx] == "Symbols Check":
+                    if val == "{}":
+                        val == ""
+                    else:
+                        val = ast.literal_eval(val)
+                        fmt_sc = ""
+                        for item in val:
+                            fmt_sc = fmt_sc + val[item]['drivers'] + '\n'
+                            fmt_sc = fmt_sc + self._fmt_driver_symbol_result(val[item]['symbol check'])
+                        val = fmt_sc
+                ws[cell_no] = val
+                ws[cell_no].border = general_border
+                ws[cell_no].alignment = general_align
+
+            supported = row['Driver Flag: supported']
+            supported = ast.literal_eval(supported)
+            df = pd.DataFrame.from_dict(supported, orient='index')
+            driver_count = df.shape[0]
+            if driver_count > 0: # format supported information
+                for sp_idx, d in df.iterrows():
+                    cell_no = xlsx_cols[supported_col_no] + str(curr_row_no + sp_idx)
+                    ws[cell_no] = d[0] + ' : ' + d[1]
+                    ws[cell_no].border = general_border
+                    ws[cell_no].alignment = general_align
+
+            if driver_count > 1: # need merge cell
+                for col_idx in range(len(cols)):
+                    if col_idx != supported_col_no:
+                        start_cell_no = xlsx_cols[col_idx] + str(curr_row_no)
+                        end_cell_no = xlsx_cols[col_idx] + str(curr_row_no+driver_count - 1)
+                        merge_range = start_cell_no + ":" + end_cell_no
+                        ws.merge_cells(merge_range)
+
+            if driver_count == 0:
+                curr_row_no += 1
+            else:
+                curr_row_no += driver_count
+
         records = len(rpm_table.index) + 1
-
-        if records < 1:
-            workbook.save(file)
-
         support_area = 'F2:F' + str(records)
         support_flag_format = self._formatting.load_support_flag_format()
         na_text = Font(color=support_flag_format['Missing']['font-color'])
@@ -142,25 +295,17 @@ class RPMsExporter:
         yes_text = Font(color=support_flag_format['yes']['font-color'])
         yes_fill = PatternFill(bgColor=support_flag_format['yes']['background-color'])
         yes = DifferentialStyle(font=yes_text, fill=yes_fill)
-        external_text = Font(color=support_flag_format['external']['font-color'])
-        external_fill = PatternFill(bgColor=support_flag_format['external']['background-color'])
-        external = DifferentialStyle(font=external_text, fill=external_fill)
 
         support_flag_na_rule = Rule(type='containsText',
                                     operator='containsText',
                                     text='Missing', dxf=na)
-        support_flag_external_rule = Rule(type='containsText',
-                                          operator='containsText',
-                                          text='external', dxf=external)
         support_flag_yes_rule = Rule(type='containsText',
                                      operator='containsText',
                                      text='yes', dxf=yes)
 
-        worksheet.conditional_formatting.add(support_area,
+        ws.conditional_formatting.add(support_area,
                                              support_flag_na_rule)
-        worksheet.conditional_formatting.add(support_area,
-                                             support_flag_external_rule)
-        worksheet.conditional_formatting.add(support_area,
+        ws.conditional_formatting.add(support_area,
                                              support_flag_yes_rule)
 
         sym_area = 'G2:G' + str(records)
@@ -171,11 +316,13 @@ class RPMsExporter:
 
         mismatch_rule = Rule(type='containsText',
                              operator='containsText',
-                             text='Symbols error founded', dxf=mismatch)
+                             text='.ko', dxf=mismatch)
 
-        worksheet.conditional_formatting.add(sym_area, mismatch_rule)
+        ws.conditional_formatting.add(sym_area, mismatch_rule)
 
-        workbook.save(file)
+        
+
+        wb.save(file)
 
     def to_pdf(self, rpm_table, file):
         self.to_html(rpm_table, '.tmp.rpms.html')
@@ -211,7 +358,8 @@ class DriversExporter:
         if value == 'yes':
             return '[green]' + value + '[/green]'
         elif value == 'external':
-            return '[blue]' + value + '[/blue]'
+            return value
+            # return '[blue]' + value + '[/blue]'
         else:
             return '[red]' + value + '[/red]'
 
@@ -231,12 +379,12 @@ class DriversExporter:
         value = value.lstrip().rstrip()
         supported = self._formatting.load_support_flag_format()
         bgcolor_yes = supported['yes']['background-color']
-        bgcolor_external = supported['external']['background-color']
+        # bgcolor_external = supported['external']['background-color']
         bgcolor_missing = supported['Missing']['background-color']
         if value == 'yes':
             return 'background-color:%s' % bgcolor_yes
-        elif value == 'external':
-            return 'background-color:%s' % bgcolor_external
+        # elif value == 'external':
+        #     return 'background-color:%s' % bgcolor_external
         elif value == 'Missing' or value == 'no':
             return 'background-color:%s' % bgcolor_missing
 
@@ -329,9 +477,9 @@ class DriversExporter:
             yes_text = Font(color=support_flag_format['yes']['font-color'])
             yes_fill = PatternFill(bgColor=support_flag_format['yes']['background-color'])
             yes = DifferentialStyle(font=yes_text, fill=yes_fill)
-            external_text = Font(color=support_flag_format['external']['font-color'])
-            external_fill = PatternFill(bgColor=support_flag_format['external']['background-color'])
-            external = DifferentialStyle(font=external_text, fill=external_fill)
+            # external_text = Font(color=support_flag_format['external']['font-color'])
+            # external_fill = PatternFill(bgColor=support_flag_format['external']['background-color'])
+            # external = DifferentialStyle(font=external_text, fill=external_fill)
             running_format = self._formatting.load_running_format()
             false_text = Font(color=running_format['false']['font-color'])
             false_fill = PatternFill(bgColor=running_format['false']['background-color'])
@@ -346,9 +494,9 @@ class DriversExporter:
             support_flag_na_rule = Rule(type='containsText',
                                         operator='containsText',
                                         text='Missing', dxf=na)
-            support_flag_external_rule = Rule(type='containsText',
-                                              operator='containsText',
-                                              text='external', dxf=external)
+            # support_flag_external_rule = Rule(type='containsText',
+            #                                   operator='containsText',
+            #                                   text='external', dxf=external)
             support_flag_yes_rule = Rule(type='containsText',
                                          operator='containsText',
                                          text='yes', dxf=yes)
@@ -363,8 +511,8 @@ class DriversExporter:
 
             worksheet.conditional_formatting.add(support_flag_area,
                                                  support_flag_na_rule)
-            worksheet.conditional_formatting.add(support_flag_area,
-                                                 support_flag_external_rule)
+            # worksheet.conditional_formatting.add(support_flag_area,
+            #                                      support_flag_external_rule)
             worksheet.conditional_formatting.add(support_flag_area,
                                                  support_flag_yes_rule)
             worksheet.conditional_formatting.add(running_area,
