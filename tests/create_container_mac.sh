deba5da64ab4609d63c7d09491b7d77e93cef4dd
Hui-Zhi <hui.zhi.zhao@suse.com>
2022-04-12 23:20:21 +0800

    remove unused import

--- tests/create_container_mac.sh
+++ tests/create_container_mac.sh
@@ -1,5 +1,5 @@
 docker run \
--it \
+-it --rm \
 --mount type=bind,source=/Users/$USER/projects/github.com/SUSE/soliddriver-checks,target=/root/source_codes \
 --mount type=bind,source=/Users/$USER/projects/Lenovo/SSDP/01-Apr-2022,target=/root/rpms \
 --mount type=bind,source=/Users/$USER/codes/tmp,target=/root/output_dir \
