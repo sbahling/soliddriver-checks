aa04e9c434b8c2ff456a5a96dedbddaf798b6bb3
Hui-Zhi <zhaohuizhi@gmail.com>
2022-12-25 12:00:36 +0800

    fix route issue

--- src/soliddriver_checks/service/service.py
+++ src/soliddriver_checks/service/service.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-from threading import Lock, Thread, Timer
-from bottle import route, run, response
+from threading import Lock, Thread
+from bottle import get, run, response
 from ..api.analysis import kms_to_json
 import os
 import json
 import logging
+from time import sleep
 
 
 class KMInfo:
@@ -18,33 +19,44 @@ class KMInfo:
 
     @property
     def data(self):
+        info = ""
         with self._data_lock:
-            data = self._info
-            return data
+            info = self._info
+        return info
 
     def _async_refresh_data(self):
         while True:
-            t = Timer(self._interval * 60 * 60, self._refresh_data)
-            t.start()
+            self._refresh_data()
+            sleep(self._interval * 60)
 
     def _refresh_data(self):
-        logging.info("start to refresh data...")
+        logger.info("start to refresh data...")
         new_data = kms_to_json()
         with self._data_lock:
             self._info = new_data
-        logging.info("refreshing data is completed!")
+        logger.info("refreshing data is completed!")
 
 
 def run_as_service(host="0.0.0.0", port=8080):
+    global logger
+    logger = logging.getLogger("soliddriver-checks-service")
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    
     interval = os.getenv("REFRESH_INTERVAL")
     interval = interval if interval is not None else 1
-    logging.info("refresh interval: %s hour(s)" % interval)
+    logger.info("refresh interval: %s hour(s)" % interval)
+    
     global kms
     kms = KMInfo(interval)
+
     run(host=host, port=port)
 
 
-@route('/kms_info')
+@get('/kms_info')
 def kms_info():
     response.content_type = 'application/json'
     info = kms.data
